28b1927b67e39609d3c513204c11a8c6eb45cd34
is108 <alexey21s@rambler.ru>
2020-06-06 01:07:09 +0700

    добавлена 3 лабораторная

---
+++ Web/Controller/QuizActionsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class QuizActionsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Quiz()
+        {
+            Operations operations = new Operations();
+            return View(operations);
+        }
+
+        [HttpPost]
+        public IActionResult Quiz(Operations operation, string action)
+        {
+            if (ModelState.IsValid)
+            {
+                CorrectAnswers answers = CorrectAnswers.Instance;
+                answers.Total += 1;
+                if (operation.Check())
+                    answers.Correct += 1;
+                (answers.Answers).Add(operation);
+            }
+            else
+            {
+                ModelState.AddModelError("YourAnswer", "недопустимый формат ответа");
+                return View(operation);
+            }
+            if (action == "Next")
+                return View(new Operations());
+            return RedirectToAction("QuizResult");
+        }
+
+        public IActionResult QuizResult()
+        {
+            CorrectAnswers answers = CorrectAnswers.Instance;
+            ViewBag.Result = answers.Answers;
+            ViewData["Correct"] = "" + answers.Correct;
+            ViewData["Total"] = "" + answers.Total;
+            return View();
+        }
+    }
+}
---
+++ Web/Models/OperationsModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    public class Operations
+    {
+        public double First { get; set; }
+        public double Second { get; set; }
+        public string Operand { get; set; }
+        public double CorrectAnswer { get; private set; }
+        [Required(ErrorMessage = "Input answer")]
+        public double Answer { get; set; }
+
+        public Operations()
+        {
+            Random random = new Random();
+            int First = random.Next(20);
+            int Second = random.Next(20);
+            string[] Operands = { "+", "-", "*", "/" };
+            this.Operand = Operands[random.Next(4)];
+            this.First = First;
+            this.Second = Second;
+        }
+        public bool Check()
+        {
+            switch (Operand)
+            {
+                case "+":
+                    CorrectAnswer = First + Second;
+                    break;
+                case "-":
+                    CorrectAnswer = First - Second;
+                    break;
+                case "*":
+                    CorrectAnswer = First * Second;
+                    break;
+                case "/":
+                    if (this.Second == 0)
+                    {
+                        this.CorrectAnswer = 0;
+                        break;
+                    }
+                    CorrectAnswer = (First / Second);
+                    break;
+            }
+            if (Answer == CorrectAnswer)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public sealed class CorrectAnswers
+    {
+        private CorrectAnswers()
+        {
+            Number = 0;
+            Correct = 0;
+            Total = 0;
+            Answers = new List<Operations>();
+        }
+        
+        public List<Operations> Answers;
+        public int Correct { get; set; }
+        public int Total { get; set; }
+        public int Number { get; set; }
+
+        public static CorrectAnswers Instance { get; } = new CorrectAnswers();
+
+    }
+}
